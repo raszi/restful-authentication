bc1488a3620532febe169d8ac16289331f889b2d
KentonWhite <jkentonwhite@gmail.com>
2011-09-14 11:42:39 -0400

    Re-written specs to conform with changes made in rspec2 and rails3

--- lib/generators/authenticated/templates/spec/helpers/users_helper_spec.rb
+++ lib/generators/authenticated/templates/spec/helpers/users_helper_spec.rb
@@ -1,52 +1,52 @@
-require File.dirname(__FILE__) + '<%= ('/..'*model_controller_class_nesting_depth) + '/../spec_helper' %>'
+require File.dirname(__FILE__) + '/../spec_helper'
 include ApplicationHelper
-include <%= model_controller_class_name %>Helper
+include UsersHelper
 include AuthenticatedTestHelper
 
-describe <%= model_controller_class_name %>Helper do
+describe UsersHelper do
   before do
-    @<%= file_name %> = mock_<%= file_name %>
+    @user = mock_user
   end
   
   describe "if_authorized" do 
     it "yields if authorized" do
-      should_receive(:authorized?).with('a','r').and_return(true)
-      if_authorized?('a','r'){|action,resource| [action,resource,'hi'] }.should == ['a','r','hi']
+      helper.should_receive(:authorized?).with('a','r').and_return(true)
+      helper.if_authorized?('a','r'){|action,resource| [action,resource,'hi'] }.should == ['a','r','hi']
     end
     it "does nothing if not authorized" do
-      should_receive(:authorized?).with('a','r').and_return(false)
-      if_authorized?('a','r'){ 'hi' }.should be_nil
+      helper.should_receive(:authorized?).with('a','r').and_return(false)
+      helper.if_authorized?('a','r'){ 'hi' }.should be_nil
     end
   end
   
-  describe "link_to_<%= file_name %>" do
-    it "should give an error on a nil <%= file_name %>" do
-      lambda { link_to_<%= file_name %>(nil) }.should raise_error('Invalid <%= file_name %>')
+  describe "link_to_user" do
+    it "should give an error on a nil user" do
+      lambda { helper.link_to_user(nil) }.should raise_error('Invalid user')
     end
-    it "should link to the given <%= file_name %>" do
-      should_receive(:<%= model_controller_routing_name.singularize %>_path).at_least(:once).and_return('/<%= model_controller_file_path %>/1')
-      link_to_<%= file_name %>(@<%= file_name %>).should have_tag("a[href='/<%= model_controller_file_path %>/1']")
+    it "should link to the given user" do
+      helper.should_receive(:user_path).at_least(:once).and_return('/users/1')
+      helper.link_to_user(@user).should have_tag("a[href='/users/1']")
     end
     it "should use given link text if :content_text is specified" do
-      link_to_<%= file_name %>(@<%= file_name %>, :content_text => 'Hello there!').should have_tag("a", 'Hello there!')
+      helper.link_to_user(@user, :content_text => 'Hello there!').should have_tag("a", :text => 'Hello there!')
     end
     it "should use the login as link text with no :content_method specified" do
-      link_to_<%= file_name %>(@<%= file_name %>).should have_tag("a", 'user_name')
+      helper.link_to_user(@user).should have_tag("a", :text => 'user_name')
     end
     it "should use the name as link text with :content_method => :name" do
-      link_to_<%= file_name %>(@<%= file_name %>, :content_method => :name).should have_tag("a", 'U. Surname')
+      helper.link_to_user(@user, :content_method => :name).should have_tag("a", :text => 'U. Surname')
     end
     it "should use the login as title with no :title_method specified" do
-      link_to_<%= file_name %>(@<%= file_name %>).should have_tag("a[title='user_name']")
+      helper.link_to_user(@user).should have_tag("a[title='user_name']")
     end
     it "should use the name as link title with :content_method => :name" do
-      link_to_<%= file_name %>(@<%= file_name %>, :title_method => :name).should have_tag("a[title='U. Surname']")
+      helper.link_to_user(@user, :title_method => :name).should have_tag("a[title='U. Surname']")
     end
     it "should have nickname as a class by default" do
-      link_to_<%= file_name %>(@<%= file_name %>).should have_tag("a.nickname")
+      helper.link_to_user(@user).should have_tag("a.nickname")
     end
     it "should take other classes and no longer have the nickname class" do
-      result = link_to_<%= file_name %>(@<%= file_name %>, :class => 'foo bar')
+      result = helper.link_to_user(@user, :class => 'foo bar')
       result.should have_tag("a.foo")
       result.should have_tag("a.bar")
     end
@@ -54,85 +54,85 @@ describe <%= model_controller_class_name %>Helper do
 
   describe "link_to_login_with_IP" do
     it "should link to the login_path" do
-      link_to_login_with_IP().should have_tag("a[href='/login']")
+      helper.link_to_login_with_IP().should have_tag("a[href='/login']")
     end
     it "should use given link text if :content_text is specified" do
-      link_to_login_with_IP('Hello there!').should have_tag("a", 'Hello there!')
+      helper.link_to_login_with_IP('Hello there!').should have_tag("a", :text => 'Hello there!')
     end
     it "should use the login as link text with no :content_method specified" do
-      link_to_login_with_IP().should have_tag("a", '0.0.0.0')
+      helper.link_to_login_with_IP().should have_tag("a", :text => '0.0.0.0')
     end
     it "should use the ip address as title" do
-      link_to_login_with_IP().should have_tag("a[title='0.0.0.0']")
+      helper.link_to_login_with_IP().should have_tag("a[title='0.0.0.0']")
     end
     it "should by default be like school in summer and have no class" do
-      link_to_login_with_IP().should_not have_tag("a.nickname")
+      helper.link_to_login_with_IP().should_not have_tag("a.nickname")
     end
     it "should have some class if you tell it to" do
-      result = link_to_login_with_IP(nil, :class => 'foo bar')
+      result = helper.link_to_login_with_IP(nil, :class => 'foo bar')
       result.should have_tag("a.foo")
       result.should have_tag("a.bar")
     end
     it "should have some class if you tell it to" do
-      result = link_to_login_with_IP(nil, :tag => 'abbr')
+      result = helper.link_to_login_with_IP(nil, :tag => 'abbr')
       result.should have_tag("abbr[title='0.0.0.0']")
     end
   end
 
-  describe "link_to_current_<%= file_name %>, When logged in" do
+  describe "link_to_current_user, When logged in" do
     before do
-      stub!(:current_<%= file_name %>).and_return(@<%= file_name %>)
+      helper.current_user = @user
     end
-    it "should link to the given <%= file_name %>" do
-      should_receive(:<%= model_controller_routing_name.singularize %>_path).at_least(:once).and_return('/<%= model_controller_file_path %>/1')
-      link_to_current_<%= file_name %>().should have_tag("a[href='/<%= model_controller_file_path %>/1']")
+    it "should link to the given user" do
+      helper.should_receive(:user_path).at_least(:once).and_return('/users/1')
+      helper.link_to_current_user().should have_tag("a[href='/users/1']")
     end
     it "should use given link text if :content_text is specified" do
-      link_to_current_<%= file_name %>(:content_text => 'Hello there!').should have_tag("a", 'Hello there!')
+      helper.link_to_current_user(:content_text => 'Hello there!').should have_tag("a", :text => 'Hello there!')
     end
     it "should use the login as link text with no :content_method specified" do
-      link_to_current_<%= file_name %>().should have_tag("a", 'user_name')
+      helper.link_to_current_user().should have_tag("a", :text => 'user_name')
     end
     it "should use the name as link text with :content_method => :name" do
-      link_to_current_<%= file_name %>(:content_method => :name).should have_tag("a", 'U. Surname')
+      helper.link_to_current_user(:content_method => :name).should have_tag("a", :text => 'U. Surname')
     end
     it "should use the login as title with no :title_method specified" do
-      link_to_current_<%= file_name %>().should have_tag("a[title='user_name']")
+      helper.link_to_current_user().should have_tag("a[title='user_name']")
     end
     it "should use the name as link title with :content_method => :name" do
-      link_to_current_<%= file_name %>(:title_method => :name).should have_tag("a[title='U. Surname']")
+      helper.link_to_current_user(:title_method => :name).should have_tag("a[title='U. Surname']")
     end
     it "should have nickname as a class" do
-      link_to_current_<%= file_name %>().should have_tag("a.nickname")
+      helper.link_to_current_user().should have_tag("a.nickname")
     end
     it "should take other classes and no longer have the nickname class" do
-      result = link_to_current_<%= file_name %>(:class => 'foo bar')
+      result = helper.link_to_current_user(:class => 'foo bar')
       result.should have_tag("a.foo")
       result.should have_tag("a.bar")
     end
   end
 
-  describe "link_to_current_<%= file_name %>, When logged out" do
+  describe "link_to_current_user, When logged out" do
     before do
-      stub!(:current_<%= file_name %>).and_return(nil)
+      helper.current_user = nil
     end
     it "should link to the login_path" do
-      link_to_current_<%= file_name %>().should have_tag("a[href='/login']")
+      helper.link_to_current_user().should have_tag("a[href='/login']")
     end
     it "should use given link text if :content_text is specified" do
-      link_to_current_<%= file_name %>(:content_text => 'Hello there!').should have_tag("a", 'Hello there!')
+      helper.link_to_current_user(:content_text => 'Hello there!').should have_tag("a", :text => 'Hello there!')
     end
     it "should use 'not signed in' as link text with no :content_method specified" do
-      link_to_current_<%= file_name %>().should have_tag("a", 'not signed in')
+      helper.link_to_current_user().should have_tag("a", :text => 'not signed in')
     end
     it "should use the ip address as title" do
-      link_to_current_<%= file_name %>().should have_tag("a[title='0.0.0.0']")
+      helper.link_to_current_user().should have_tag("a[title='0.0.0.0']")
     end
     it "should by default be like school in summer and have no class" do
-      link_to_current_<%= file_name %>().should_not have_tag("a.nickname")
+      helper.link_to_current_user().should_not have_tag("a.nickname")
     end
     it "should have some class if you tell it to" do
-      result = link_to_current_<%= file_name %>(:class => 'foo bar')
+      result = helper.link_to_current_user(:class => 'foo bar')
       result.should have_tag("a.foo")
       result.should have_tag("a.bar")
     end
--- lib/generators/authenticated/templates/spec/models/user_spec.rb
+++ lib/generators/authenticated/templates/spec/models/user_spec.rb
@@ -1,38 +1,26 @@
 # -*- coding: utf-8 -*-
-require File.dirname(__FILE__) + '<%= ('/..'*model_controller_class_nesting_depth) + '/../spec_helper' %>'
+require File.dirname(__FILE__) + '/../spec_helper'
 
 # Be sure to include AuthenticatedTestHelper in spec/spec_helper.rb instead.
 # Then, you can remove it from this and the functional test.
 include AuthenticatedTestHelper
 
-describe <%= class_name %> do
-  fixtures :<%= table_name %>
+describe User do
+  fixtures :users
 
   describe 'being created' do
     before do
-      @<%= file_name %> = nil
-      @creating_<%= file_name %> = lambda do
-        @<%= file_name %> = create_<%= file_name %>
-        violated "#{@<%= file_name %>.errors.full_messages.to_sentence}" if @<%= file_name %>.new_record?
+      @user = nil
+      @creating_user = lambda do
+        @user = create_user
+        violated "#{@user.errors.full_messages.to_sentence}" if @user.new_record?
       end
     end
 
-    it 'increments <%= class_name %>#count' do
-      @creating_<%= file_name %>.should change(<%= class_name %>, :count).by(1)
+    it 'increments User#count' do
+      @creating_user.should change(User, :count).by(1)
     end
-<% if options[:include_activation] %>
-    it 'initializes #activation_code' do
-      @creating_<%= file_name %>.call
-      @<%= file_name %>.reload
-      @<%= file_name %>.activation_code.should_not be_nil
-    end
-<% end %><% if options[:stateful] %>
-    it 'starts in pending state' do
-      @creating_<%= file_name %>.call
-      @<%= file_name %>.reload
-      @<%= file_name %>.should be_pending
-    end
-<% end %>  end
+  end
 
   #
   # Validations
@@ -40,9 +28,9 @@ describe <%= class_name %> do
 
   it 'requires login' do
     lambda do
-      u = create_<%= file_name %>(:login => nil)
-      u.errors.on(:login).should_not be_nil
-    end.should_not change(<%= class_name %>, :count)
+      u = create_user(:login => nil)
+      u.errors.get(:login).should_not be_nil
+    end.should_not change(User, :count)
   end
 
   describe 'allows legitimate logins:' do
@@ -50,9 +38,9 @@ describe <%= class_name %> do
      'hello.-_there@funnychar.com'].each do |login_str|
       it "'#{login_str}'" do
         lambda do
-          u = create_<%= file_name %>(:login => login_str)
-          u.errors.on(:login).should     be_nil
-        end.should change(<%= class_name %>, :count).by(1)
+          u = create_user(:login => login_str)
+          u.errors.get(:login).should     be_nil
+        end.should change(User, :count).by(1)
       end
     end
   end
@@ -62,32 +50,32 @@ describe <%= class_name %> do
      'semicolon;', 'quote"', 'tick\'', 'backtick`', 'percent%', 'plus+', 'space '].each do |login_str|
       it "'#{login_str}'" do
         lambda do
-          u = create_<%= file_name %>(:login => login_str)
-          u.errors.on(:login).should_not be_nil
-        end.should_not change(<%= class_name %>, :count)
+          u = create_user(:login => login_str)
+          u.errors.get(:login).should_not be_nil
+        end.should_not change(User, :count)
       end
     end
   end
 
   it 'requires password' do
     lambda do
-      u = create_<%= file_name %>(:password => nil)
-      u.errors.on(:password).should_not be_nil
-    end.should_not change(<%= class_name %>, :count)
+      u = create_user(:password => nil)
+      u.errors.get(:password).should_not be_nil
+    end.should_not change(User, :count)
   end
 
   it 'requires password confirmation' do
     lambda do
-      u = create_<%= file_name %>(:password_confirmation => nil)
-      u.errors.on(:password_confirmation).should_not be_nil
-    end.should_not change(<%= class_name %>, :count)
+      u = create_user(:password_confirmation => nil)
+      u.errors.get(:password_confirmation).should_not be_nil
+    end.should_not change(User, :count)
   end
 
   it 'requires email' do
     lambda do
-      u = create_<%= file_name %>(:email => nil)
-      u.errors.on(:email).should_not be_nil
-    end.should_not change(<%= class_name %>, :count)
+      u = create_user(:email => nil)
+      u.errors.get(:email).should_not be_nil
+    end.should_not change(User, :count)
   end
 
   describe 'allows legitimate emails:' do
@@ -98,9 +86,9 @@ describe <%= class_name %> do
     ].each do |email_str|
       it "'#{email_str}'" do
         lambda do
-          u = create_<%= file_name %>(:email => email_str)
-          u.errors.on(:email).should     be_nil
-        end.should change(<%= class_name %>, :count).by(1)
+          u = create_user(:email => email_str)
+          u.errors.get(:email).should     be_nil
+        end.should change(User, :count).by(1)
       end
     end
   end
@@ -113,9 +101,9 @@ describe <%= class_name %> do
     ].each do |email_str|
       it "'#{email_str}'" do
         lambda do
-          u = create_<%= file_name %>(:email => email_str)
-          u.errors.on(:email).should_not be_nil
-        end.should_not change(<%= class_name %>, :count)
+          u = create_user(:email => email_str)
+          u.errors.get(:email).should_not be_nil
+        end.should_not change(User, :count)
       end
     end
   end
@@ -126,9 +114,9 @@ describe <%= class_name %> do
     ].each do |name_str|
       it "'#{name_str}'" do
         lambda do
-          u = create_<%= file_name %>(:name => name_str)
-          u.errors.on(:name).should     be_nil
-        end.should change(<%= class_name %>, :count).by(1)
+          u = create_user(:name => name_str)
+          u.errors.get(:name).should     be_nil
+        end.should change(User, :count).by(1)
       end
     end
   end
@@ -138,50 +126,50 @@ describe <%= class_name %> do
      ].each do |name_str|
       it "'#{name_str}'" do
         lambda do
-          u = create_<%= file_name %>(:name => name_str)
-          u.errors.on(:name).should_not be_nil
-        end.should_not change(<%= class_name %>, :count)
+          u = create_user(:name => name_str) 
+          u.errors.get(:name).should_not be_nil
+        end.should_not change(User, :count)
       end
     end
   end
 
   it 'resets password' do
-    <%= table_name %>(:quentin).update_attributes(:password => 'new password', :password_confirmation => 'new password')
-    <%= class_name %>.authenticate('quentin', 'new password').should == <%= table_name %>(:quentin)
+    users(:quentin).update_attributes(:password => 'new password', :password_confirmation => 'new password')
+    User.authenticate('quentin', 'new password').should == users(:quentin)
   end
 
   it 'does not rehash password' do
-    <%= table_name %>(:quentin).update_attributes(:login => 'quentin2')
-    <%= class_name %>.authenticate('quentin2', 'monkey').should == <%= table_name %>(:quentin)
+    users(:quentin).update_attributes(:login => 'quentin2')
+    User.authenticate('quentin2', 'monkey').should == users(:quentin)
   end
 
   #
   # Authentication
   #
 
-  it 'authenticates <%= file_name %>' do
-    <%= class_name %>.authenticate('quentin', 'monkey').should == <%= table_name %>(:quentin)
+  it 'authenticates user' do
+    User.authenticate('quentin', 'monkey').should == users(:quentin)
   end
 
-  it "doesn't authenticate <%= file_name %> with bad password" do
-    <%= class_name %>.authenticate('quentin', 'invalid_password').should be_nil
+  it "doesn't authenticate user with bad password" do
+    User.authenticate('quentin', 'invalid_password').should be_nil
   end
 
  if REST_AUTH_SITE_KEY.blank?
    # old-school passwords
    it "authenticates a user against a hard-coded old-style password" do
-     <%= class_name %>.authenticate('old_password_holder', 'test').should == <%= table_name %>(:old_password_holder)
+     User.authenticate('old_password_holder', 'test').should == users(:old_password_holder)
    end
  else
    it "doesn't authenticate a user against a hard-coded old-style password" do
-     <%= class_name %>.authenticate('old_password_holder', 'test').should be_nil
+     User.authenticate('old_password_holder', 'test').should be_nil
    end
 
    # New installs should bump this up and set REST_AUTH_DIGEST_STRETCHES to give a 10ms encrypt time or so
    desired_encryption_expensiveness_ms = 0.1
    it "takes longer than #{desired_encryption_expensiveness_ms}ms to encrypt a password" do
      test_reps = 100
-     start_time = Time.now; test_reps.times{ <%= class_name %>.authenticate('quentin', 'monkey'+rand.to_s) }; end_time   = Time.now
+     start_time = Time.now; test_reps.times{ User.authenticate('quentin', 'monkey'+rand.to_s) }; end_time   = Time.now
      auth_time_ms = 1000 * (end_time - start_time)/test_reps
      auth_time_ms.should > desired_encryption_expensiveness_ms
    end
@@ -192,99 +180,48 @@ describe <%= class_name %> do
   #
 
   it 'sets remember token' do
-    <%= table_name %>(:quentin).remember_me
-    <%= table_name %>(:quentin).remember_token.should_not be_nil
-    <%= table_name %>(:quentin).remember_token_expires_at.should_not be_nil
+    users(:quentin).remember_me
+    users(:quentin).remember_token.should_not be_nil
+    users(:quentin).remember_token_expires_at.should_not be_nil
   end
 
   it 'unsets remember token' do
-    <%= table_name %>(:quentin).remember_me
-    <%= table_name %>(:quentin).remember_token.should_not be_nil
-    <%= table_name %>(:quentin).forget_me
-    <%= table_name %>(:quentin).remember_token.should be_nil
+    users(:quentin).remember_me
+    users(:quentin).remember_token.should_not be_nil
+    users(:quentin).forget_me
+    users(:quentin).remember_token.should be_nil
   end
 
   it 'remembers me for one week' do
     before = 1.week.from_now.utc
-    <%= table_name %>(:quentin).remember_me_for 1.week
+    users(:quentin).remember_me_for 1.week
     after = 1.week.from_now.utc
-    <%= table_name %>(:quentin).remember_token.should_not be_nil
-    <%= table_name %>(:quentin).remember_token_expires_at.should_not be_nil
-    <%= table_name %>(:quentin).remember_token_expires_at.between?(before, after).should be_true
+    users(:quentin).remember_token.should_not be_nil
+    users(:quentin).remember_token_expires_at.should_not be_nil
+    users(:quentin).remember_token_expires_at.between?(before, after).should be_true
   end
 
   it 'remembers me until one week' do
     time = 1.week.from_now.utc
-    <%= table_name %>(:quentin).remember_me_until time
-    <%= table_name %>(:quentin).remember_token.should_not be_nil
-    <%= table_name %>(:quentin).remember_token_expires_at.should_not be_nil
-    <%= table_name %>(:quentin).remember_token_expires_at.should == time
+    users(:quentin).remember_me_until time
+    users(:quentin).remember_token.should_not be_nil
+    users(:quentin).remember_token_expires_at.should_not be_nil
+    users(:quentin).remember_token_expires_at.should == time
   end
 
   it 'remembers me default two weeks' do
     before = 2.weeks.from_now.utc
-    <%= table_name %>(:quentin).remember_me
+    users(:quentin).remember_me
     after = 2.weeks.from_now.utc
-    <%= table_name %>(:quentin).remember_token.should_not be_nil
-    <%= table_name %>(:quentin).remember_token_expires_at.should_not be_nil
-    <%= table_name %>(:quentin).remember_token_expires_at.between?(before, after).should be_true
-  end
-<% if options[:stateful] %>
-  it 'registers passive <%= file_name %>' do
-    <%= file_name %> = create_<%= file_name %>(:password => nil, :password_confirmation => nil)
-    <%= file_name %>.should be_passive
-    <%= file_name %>.update_attributes(:password => 'new password', :password_confirmation => 'new password')
-    <%= file_name %>.register!
-    <%= file_name %>.should be_pending
+    users(:quentin).remember_token.should_not be_nil
+    users(:quentin).remember_token_expires_at.should_not be_nil
+    users(:quentin).remember_token_expires_at.between?(before, after).should be_true
   end
 
-  it 'suspends <%= file_name %>' do
-    <%= table_name %>(:quentin).suspend!
-    <%= table_name %>(:quentin).should be_suspended
-  end
-
-  it 'does not authenticate suspended <%= file_name %>' do
-    <%= table_name %>(:quentin).suspend!
-    <%= class_name %>.authenticate('quentin', 'monkey').should_not == <%= table_name %>(:quentin)
-  end
-
-  it 'deletes <%= file_name %>' do
-    <%= table_name %>(:quentin).deleted_at.should be_nil
-    <%= table_name %>(:quentin).delete!
-    <%= table_name %>(:quentin).deleted_at.should_not be_nil
-    <%= table_name %>(:quentin).should be_deleted
-  end
-
-  describe "being unsuspended" do
-    fixtures :<%= table_name %>
-
-    before do
-      @<%= file_name %> = <%= table_name %>(:quentin)
-      @<%= file_name %>.suspend!
-    end
-
-    it 'reverts to active state' do
-      @<%= file_name %>.unsuspend!
-      @<%= file_name %>.should be_active
-    end
-
-    it 'reverts to passive state if activation_code and activated_at are nil' do
-      <%= class_name %>.update_all :activation_code => nil, :activated_at => nil
-      @<%= file_name %>.reload.unsuspend!
-      @<%= file_name %>.should be_passive
-    end
-
-    it 'reverts to pending state if activation_code is set and activated_at is nil' do
-      <%= class_name %>.update_all :activation_code => 'foo-bar', :activated_at => nil
-      @<%= file_name %>.reload.unsuspend!
-      @<%= file_name %>.should be_pending
-    end
-  end
-<% end %>
 protected
-  def create_<%= file_name %>(options = {})
-    record = <%= class_name %>.new({ :login => 'quire', :email => 'quire@example.com', :password => 'quire69', :password_confirmation => 'quire69' }.merge(options))
-    record.<% if options[:stateful] %>register! if record.valid?<% else %>save<% end %>
+  def create_user(options = {})
+    record = User.new({ :login => 'quire', :email => 'quire@example.com', :password => 'quire69', :password_confirmation => 'quire69' }.merge(options))
+    record.save
     record
   end
 end
